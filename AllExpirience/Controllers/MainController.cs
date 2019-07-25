52f1c7d85e5206f080c6bb5b50ec7e19b4e7ca33
Artiom Krukovsky <Artiomgood1@gmail.com>
2019-07-25 16:50:42 +0300

    InitCommit

---
+++ AllExpirience/Controllers/MainController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AllExpirience.Controllers
+{
+    public class MainController : Controller
+    {
+
+        public ActionResult Home()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+
+        public ActionResult OtherInformation()
+        {
+            return View();
+        }
+
+        public ActionResult AboutMeProgramming()
+        {
+            return View();
+        }
+    }
+}
